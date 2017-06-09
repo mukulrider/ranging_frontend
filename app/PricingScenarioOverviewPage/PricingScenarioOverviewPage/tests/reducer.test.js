459984a23ad07cc0be93616fd5777e5f31ec45b3
harshit.bansal <harshit.bansal@mu-sigma.com>
2017-06-09 17:01:43 +0530

    Refactoring: removing Pricing containers from app

--- app/PricingScenarioOverviewPage/PricingScenarioOverviewPage/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import pricingScenarioOverviewPageReducer from '../reducer';
-
-describe('pricingScenarioOverviewPageReducer', () => {
-  it('returns the initial state', () => {
-    expect(pricingScenarioOverviewPageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- app/containers/PricingForecastScenario/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import pricingForecastScenarioReducer from '../reducer';
-
-describe('pricingForecastScenarioReducer', () => {
-  it('returns the initial state', () => {
-    expect(pricingForecastScenarioReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
